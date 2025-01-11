3752f18d5dbef8f1b0a301a6625366a56c5fb5a6
jw <jwfnov@gmail.com>
2025-01-12 02:01:34 +0800

    edit fileexplorer.lua to add keymap to toggle :Neotree and :Neotree reveal window

--- lua/plugins/fileexplorer.lua
+++ lua/plugins/fileexplorer.lua
@@ -7,7 +7,31 @@ plugin =
           "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
           "MunifTanjim/nui.nvim",
           -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-        }
+        },
+	cmd = 'Neotree',  -- ensure neotrim plugin is loaded properly before any key mappings is used.
+	-- after adding the "keys" section, neovim would only lazy-load the command :Neotree and the mapped key wouldn't trigger the load. So it becomes neccessary to add the "cmd='Neotree' before keys section.
+	keys = {
+		{
+			'\\', ':Neotree<CR>',
+			desc = 'Open Noetree',
+			silent = true,
+		},
+		{
+			'<C-\\>', ':Neotree reveal<CR>',
+			desc = 'Open Noetree reveal current file',
+			silent = true,
+		},
+	},
+	--while 'keys' is for whole neovim, opts mappings is for neotree only
+	opts = {
+		filesystem = {
+			window = {
+				mappings = {
+					['\\'] = 'close_window',
+				},
+			},
+		},
+	},
     }
 
 return plugin

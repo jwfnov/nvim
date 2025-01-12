4b81360bb0ecbcc3ec69886844a9651ecd297c3e
jw <jwfnov@gmail.com>
2025-01-12 23:40:05 +0800

    edit lua/jw/options.lua to make lua files indentation to 2 spaces

--- lua/jw/options.lua
+++ lua/jw/options.lua
@@ -3,3 +3,16 @@ local o = vim.opt
 o.number = true
 o.relativenumber = true
 
+
+--Set Indentation for Lua Files
+vim.api.nvim_create_autocmd(
+	"FileType", 
+	{
+		pattern = "lua",
+		callback = function()
+			vim.opt_local.tabstop = 2
+			vim.opt_local.shiftwidth = 2
+			vim.opt_local.expandtab = true
+		end,
+	}
+)

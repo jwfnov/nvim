4727bef86d31319b5141551787016e8f78b6e4e0
jw <jwfnov@gmail.com>
2025-01-19 16:23:59 +0800

    add plugins.pyright to detect project .venv to get rid of 'import not resolved' error

--- lua/plugins/nvim_lspconfig.lua
+++ lua/plugins/nvim_lspconfig.lua
@@ -13,9 +13,20 @@ plugin = {
   config = function() 
     --=== set up each lsp server in nvim lspconfig
     local nvim_lspconfig = require("lspconfig")
-
+    
+    --=pylsp
     nvim_lspconfig.pylsp.setup({})  -- goto definitons/references
-    nvim_lspconfig.pyright.setup({})  -- import not resolved/pep standard
+    
+    --=pyright
+    local func_setup_pyright = require("plugins.pyright")
+    --nvim_lspconfig.pyright.setup({})  -- import not resolved/pep standard
+    func_setup_pyright(nvim_lspconfig)
+
+    -- Add a keymap to trigger the .venv folder check and set it as the venv
+    -- NOTE: the require must have the path corresponding to your choice of folder structure
+    -- TRICK: require twice here, the 2nd one is passing lspconfig into the function so we can use the nested function "set_venv()" which is inside the table returned by func_setup_pyright() so we can call it
+    vim.api.nvim_set_keymap('n', '<leader>venv', '<cmd>lua require("plugins.pyright")(require("lspconfig")).set_venv()<CR>', { noremap = true, silent = true, desc = "Set .venv for pyright" })
+
   end,
 }
 
